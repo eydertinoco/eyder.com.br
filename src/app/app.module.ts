aea5c4b35c1fbe021cad76f3c82e40f83342dc50
Eyder <eydertinoco@outlook.com>
2024-01-15 16:11:34 -0300

    Estilos funcionando;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HomeComponent} from "./module/home/home.component";
+import {AppHeaderComponent} from "./layout/app-header/app-header.component";
+import {AppFooterComponent} from "./layout/app-footer/app-footer.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AppHeaderComponent,
+    AppFooterComponent
   ],
   imports: [
     BrowserModule,
