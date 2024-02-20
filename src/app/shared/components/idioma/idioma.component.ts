f16afceabf68f67a0e7401be79f7ac09b9ee5bac
eyder <eydertinoco@outlook.com>
2024-02-20 07:33:32 -0300

    Adicionado robots

---
+++ src/app/shared/components/idioma/idioma.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-idioma',
+    templateUrl: './idioma.component.html',
+    styleUrls: ['./idioma.component.scss'],
+
+})
+
+export class IdiomaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+
+}
